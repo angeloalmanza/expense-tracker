6963f4e9f44630f02feb65c45d4ff2baa2a026a9
angeloalmanza <angeloalmanza26@gmail.com>
2026-02-16 16:13:23 +0100

    feat: auto-clear AI insights cache on transaction changes

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTransactionRequest;
 use App\Http\Requests\UpdateTransactionRequest;
+use App\Services\AiInsightsService;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
+    public function __construct(private AiInsightsService $insights) {}
+
     public function index(Request $request)
     {
         $transactions = $request->user()->transactions()->latest()->get();
@@ -17,6 +20,7 @@ class TransactionController extends Controller
     public function store(StoreTransactionRequest $request)
     {
         $transaction = $request->user()->transactions()->create($request->validated());
+        $this->insights->clearCache($request->user());
 
         return response()->json($transaction, 201);
     }
@@ -25,6 +29,7 @@ class TransactionController extends Controller
     {
         $transaction = $request->user()->transactions()->findOrFail($id);
         $transaction->update($request->validated());
+        $this->insights->clearCache($request->user());
 
         return response()->json($transaction);
     }
@@ -33,6 +38,7 @@ class TransactionController extends Controller
     {
         $transaction = $request->user()->transactions()->findOrFail($id);
         $transaction->delete();
+        $this->insights->clearCache($request->user());
 
         return response()->json(null, 204);
     }
